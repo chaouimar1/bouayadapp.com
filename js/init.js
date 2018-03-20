a1ad239f4d28d6f1bea386881d324847fb6dfae4
Marwane XD <chaoui.mar2@gmail.com>
2018-03-20 19:19:43 +0000

    animate css

--- js/init.js
+++ js/init.js
@@ -3,5 +3,30 @@
     
     $('.materialboxed').materialbox();
 
+    var options = [
+      {
+        selector: '.about_section', offset: 200, callback: function (el) {
+          $(el).addClass('fadeInUp');
+        }
+      },
+      {
+        selector: '.developer_section', offset: 200, callback: function (el) {
+          $(el).addClass('fadeInUp');
+        }
+      },
+      {
+        selector: '.header_section', offset: 200, callback: function (el) {
+          $(el).addClass('fadeInUp');
+        }
+      },
+      {
+        selector: '.screenshots_section', offset: 200, callback: function (el) {
+          $(el).addClass('fadeInUp');
+        }
+      }
+    ]; 
+    
+    Materialize.scrollFire(options); 
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
